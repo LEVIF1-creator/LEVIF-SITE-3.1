0428ba7c3ccd01c4f181800f4f68939b8813bbb8
LEVIF1-creator <lecocqerick1@gmail.com>
2026-04-10 15:12:15 +0200

    Create publish-actualite.js

---
+++ api/publish-actualite.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const data = req.body;
+
+  console.log('Nouvelle actu reçue :', data);
+
+  return res.status(200).json({
+    success: true
+  });
+}
