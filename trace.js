0044aaa5406572c987fe795610d046aa7cb7c371
LEVIF1-creator <lecocqerick1@gmail.com>
2026-05-22 19:03:45 +0200

    Update trace.js

--- trace.js
+++ trace.js
@@ -1,11 +1,14 @@
-let globalCount = 248;
-
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    globalCount++;
-  }
 
-  res.status(200).json({
-    count: globalCount
+  const response = await fetch("TON_WEBHOOK_MAKE", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req.body)
   });
+
+  const data = await response.json();
+
+  res.status(200).json(data);
 }
