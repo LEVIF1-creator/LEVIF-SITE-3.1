3b06d1af706536ee8829c6f4d6629f1147257a64
LEVIF1-creator <lecocqerick1@gmail.com>
2026-05-22 19:06:20 +0200

    Update trace.js

--- trace.js
+++ trace.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
 
-  const response = await fetch("TON_WEBHOOK_MAKE", {
+  const response = await fetch("https://hook.eu1.make.com/fw2lb6cqupenxbq2jmfq6p6669kcy7sp", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
