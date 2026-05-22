1817a21de4b834d053a4a30d359c39f7663f2895
LEVIF1-creator <lecocqerick1@gmail.com>
2026-05-22 10:59:05 +0200

    Update trace.js

--- trace.js
+++ trace.js
@@ -1,1 +1,11 @@
+let globalCount = 248;
 
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    globalCount++;
+  }
+
+  res.status(200).json({
+    count: globalCount
+  });
+}
